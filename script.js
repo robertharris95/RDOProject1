7efb8965e42910dce6e2309304fd5b72b9522740
Robert Harris <rlh9@rice.edu>
2020-02-19 16:16:45 -0600

    Populates new artist, other artists, and the artists music along with links

--- script.js
+++ script.js
@@ -3,66 +3,50 @@ $(".searchBtn").on("click",function(e) {
 e.preventDefault();
 
 // Variables
-
+var input = $(".search").val();
+input = input.replace(/\s/g,'').toLowerCase();
 //Call using page input to get genre
-var urlHeartArt = "https://api-v2.hearthis.at/shawne/?type=likes&page=1&count=5"
+var urlHeartArt = "https://api-v2.hearthis.at/"+ input +"/?type=likes&page=1&count=5"
 
 $.ajax({
   url:urlHeartArt,
   method:"GET"
   
 }).then(function(response){
-  console.log(response[0].genre)
+//   console.log(response)
   var genre = response[0].genre
+  genre = genre.replace(/\s/g,'').toLowerCase();
 
 // Call using genre to get random artist 
+
 var urlHeart =  "https://api-v2.hearthis.at/categories/"+genre+"/?page="+Math.floor(Math.random()*10)+"&count="+Math.floor(Math.random() * 10);
   $.ajax({
     url: urlHeart,
      method: "GET"
-}).then(function (response) {
+}).then(function(response) {
     //Variables for the users artistName and Description
-    var artistName = response[0].user.username;
+    console.log(response);
+    var artistname = response[0].user.username;
     var description = response[0].user.caption
     //Change description to description from response
    $(".artist-describe").text(description)
- 
- 
-// Call using random artist name to get BIT info
-var urlBIT = "https://rest.bandsintown.com/artists/"+ artistname + "/events?app_id=aaa3bc3b24af93f2b09a687107fc37f5&date=upcoming"
-$.ajax({
-    url: urlBIT,
-    method: "GET"
-  
-}).then(function (response) {
-    $(".artist-name").text(response[0].artist.name)
-    $(".artist-image").attr("src",response[0].artist.thumb_url)
-    $(".mainartistBtn").attr("href",response[0].url)
+   $(".artist-name").text(artistname)
 
+   $(".artist-image").attr("src",response[0].thumb)
+   $(".mainartistBtn").attr("href",response[0].permalink_url)
+    $(".player").attr("src", response[0].preview_url)
+
+   $(".similarartist1").attr("src",response[1].thumb)
+   $(".similarArtistButton1").attr("href",response[1].permalink_url)
+   $(".similarArtistname1").text(response[1].user.username)
+
+   $(".similarartist2").attr("src",response[2].thumb)
+   $(".similarArtistButton2").attr("href",response[2].permalink_url)
+   $(".similarArtistname2").text(response[2].user.username)
+
+   $(".similarartist3").attr("src",response[3].thumb)
+   $(".similarArtistButton3").attr("href",response[3].permalink_url)
+   $(".similarArtistname3").text(response[3].user.username)
 })
-
- 
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+});

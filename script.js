2027230e21aff6ca3d0a0c54c3626be744f31f98
Ose <38203116+Ghalileo@users.noreply.github.com>
2020-02-19 13:43:35 -0600

    added-comments-to-ajax

--- script.js
+++ script.js
@@ -30,10 +30,13 @@ $.ajax({
     method: "GET"
 
 }).then(function (response) {
+    //Variables for the users artistName and Description
     var artistName = response[0].user.username;
     var description = response[0].user.caption;
+    //Log test of the artist name matching and being correctly defined
     console.log(response[0].user.username);
     console.log(artistName);
+    //Log test of the artist description matching and being correctly defined
     console.log(response[0].user.caption);
     console.log(description);
 

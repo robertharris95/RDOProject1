19ac9ac9d5a4d3c11969e2edc1569a0245921e67
Robert Harris <rlh9@rice.edu>
2020-02-19 13:45:49 -0600

    3rd ajax complete, awaiting merge

--- script.js
+++ script.js
@@ -1,56 +1,73 @@
 
 
 
+// On clicking the search button 
+$(".searchBtn").on("click",function(e) {
+e.preventDefault();
 
-$("search").click(function () {
-
-
-
-
-})
-
-var urlSpot = "" 
+// Variables
 
 
 
 
 
 
+// Call using artist to get genre 
+// $.ajax({
+//     url: urlHeart,
+//     method: "GET"
 
+// }).then(function(response) {
+
+    // console.log(response);
+
+    // $(".media-object-section").text("")
+    // $(".similarArtistname").text("")
+    // $(".similarArtistDescribe").text("")
+
+
+// })
+
+// Call using genre to get random artist 
+// $.ajax({
+//     url: urlHeart,
+//     method: "GET"
+
+// }).then(function(response) {
+
+    // console.log(response);
+
+    // $(".media-object-section").text("")
+    // $(".similarArtistname").text("")
+    // $(".similarArtistDescribe").text("")
+
+
+// })
+
+// Call using random artist name to get BIT info
+var artistname= "trippiered"
+var urlBIT = "https://rest.bandsintown.com/artists/"+ artistname + "/events?app_id=aaa3bc3b24af93f2b09a687107fc37f5&date=upcoming"
 $.ajax({
-    url: urlHeart,
+    url: urlBIT,
     method: "GET"
+  
+}).then(function(response) {
+   console.log(response)
 
-}).then(function (urlHeart) {
-
-    console.log(urlHeart);
-
-    $("#media-object-section").text("")
-    $("#similarArtistname").text("")
-    $("#similarArtistDescribe").text("")
+    $(".artist-name").text(response[0].artist.name)
+    $(".artist-image").attr("src",response[0].artist.thumb_url)
+    $(".mainartistBtn").attr("href",response[0].url)
 
 
 })
 
-
-var urlBIT = ""
-
+    // $(".artist-describe").text(response[0].user.caption)
 
 
-$.ajax({
-    url: url,
-    method: "GET"
-
-}).then(function (urlBIT) {
-
-    console.log(urlBIT);
-
-    $("#media-object-section").text("")
-    $("#similarArtistname").text("")
-    $("#similarArtistDescribe").text("")
 
 
-})
+});
+
 
 
 
